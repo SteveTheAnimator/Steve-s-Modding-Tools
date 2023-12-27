702893c567f92a1c6dbb2b1368f1deb35e258b1b
Steve The Animator <103543324+SteveTheAnimator@users.noreply.github.com>
2023-12-27 07:37:12 -0700

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace StevesModdingTools
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.Steve.gorillatag.StevesModdingTools";
+		public const string Name = "Steve's Modding Tools";
+		public const string Version = "1.0.0";
+	}
+}
